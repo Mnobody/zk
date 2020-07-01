44dc479668fd4bf56f07b47bdffbfe05fae255b8
mnobody <wojtowicz.wiktor13@gmail.com>
2020-07-01 20:07:59 +0200

    remove unnecessary annotations

--- src/User/Identity.php
+++ src/User/Identity.php
@@ -5,15 +5,9 @@ declare(strict_types=1);
 namespace App\User;
 
 use Yiisoft\Auth\IdentityInterface;
-use Cycle\Annotated\Annotation\Entity;
-use Cycle\Annotated\Annotation\Column;
 
-/**
- * @Entity(repository = "App\User\IdentityRepository")
- */
 final class Identity implements IdentityInterface
 {
-    /** @Column(type = "bigPrimary") */
     private string $id;
 
     public function __construct(string $id) {
